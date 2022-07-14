722cd3e8e13fdda89560aa7a7c4fe40910410151
Alex <sturmmanngr@gmail.com>
2022-07-14 16:43:25 +0700

    start working on blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE)
+    body = models.TextField(blank=True)
+    publish = models.DateTimeField(blank=True, default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(blank=True, max_length=10, choices=STATUS_CHOICES, default='draft')
+
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                         args=[
+                         self.publish.strftime('%m'),
+                         self.strftime('%d'),
+                         self.slug])
+
+    def __str__(self):
+        return self.title
